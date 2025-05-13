d50c5a0c765d738c9183f1ead9aad68a1750b963
SaifUllah <47953075+saifullahakhtar@users.noreply.github.com>
2025-05-13 17:45:31 +0400

    Project Staging 17

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
 	experimental: {
 		missingSuspenseWithCSRBailout: false,
 	},
+	reactStrictMode: true,
+	experimental: {
+		appDir: true, // This is for Next.js App Directory
+	},
 };
 
 export default nextConfig;
