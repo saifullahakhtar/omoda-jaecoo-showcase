642ccb02217681fabe7628a0978c80bf8dfb6cf3
SaifUllah <47953075+saifullahakhtar@users.noreply.github.com>
2025-05-14 11:46:22 +0400

    Project Staging 23

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,17 +2,17 @@
 
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-// import Lottie from "react-lottie-player";
+import Lottie from "react-lottie-player";
 import { useProgress } from "@react-three/drei";
 
-// import loadingLottieJson from "@/components/lottie/loading-progress.json";
+import loadingLottieJson from "@/components/lottie/loading-progress.json";
 
 const SceneNoSSR = dynamic(() => import("@/components/scene/Scene"), {
 	ssr: false,
 	loading: () => null,
 });
 
-// import lottieJson from "@/components/lottie/rotate.json";
+import lottieJson from "@/components/lottie/rotate.json";
 
 const Home = () => {
 	const [isLandscape, setIsLandscape] = useState(false);
@@ -52,24 +52,24 @@ const Home = () => {
 		<main className="h-screen w-screen">
 			{!isLandscape ? (
 				<div className="grid place-content-center p-8 h-screen w-screen">
-					{/* <Lottie
+					<Lottie
 						loop
 						animationData={lottieJson}
 						play
 						style={{ width: 200, height: 200 }}
-					/> */}
+					/>
 				</div>
 			) : (
 				<>
 					{isLoading && (
 						<div className="fixed top-0 left-0 w-screen h-screen z-50 flex justify-center items-center bg-black">
 							<div className="text-center">
-								{/* <Lottie
+								<Lottie
 									loop
 									animationData={loadingLottieJson}
 									play
 									style={{ width: 150, height: 150 }}
-								/> */}
+								/>
 							</div>
 							<p className="fixed bottom-5 lg:bottom-8 right-5 lg:right-8 text-xl lg:text-4xl uppercase font-normal italic text-white z-50">
 								{Math.round(progress)}%
